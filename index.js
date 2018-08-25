7fa734d214fb2e60d59d44bc729256f99a2df24f
Pablo Eusebio <nunieboy88@gmail.com>
2018-08-25 12:28:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string){
-  if string === string.toLowerCase()
-  return console.log("I can't hear you")
-}
+  if string === string.toLowerCase(){
+      return 
+  }
